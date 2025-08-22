f8bc6983d1e0821868d56126e005ac0c91afc08e
Rohit Ware <rohitware99@gmail.com>
2025-08-22 21:28:02 +0530

    Provide the Redux Store(Counter store) to React

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import store from "./CRUDUsingRedux/Store.js";
+// import store from "./CRUDUsingRedux/Store.js";
+import { counterStore } from "./CounterApp/counterStore.js";
 import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
+  <Provider store={counterStore}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
